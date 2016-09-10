c4833eda55e4d21717d7932697dcd054878cf422
mailhap <mailhap@qq.com>
2016-09-10 21:20:29 +0800

    ignore build dir

--- src/page/egg/index.js
+++ src/page/egg/index.js
@@ -1,35 +1,34 @@
 import Vue from 'vue'
-import $ from 'zeptor'
 
-var panes = [
-	{
-		ready: function(el){
-			new Vue($.extend({
-				el: el,
-			}, require('./game2.vue')));
-		}
-	},
-	{
-		ready: function(el){
-			new Vue($.extend({
-				el: el,
-			}, require('./game1.vue')));
-		}
-	}
-];
+// var panes = [
+// 	{
+// 		ready: function(el){
+// 			new Vue(Object.assign({
+// 				el: el,
+// 			}, require('./game2.vue')));
+// 		}
+// 	},
+// 	{
+// 		ready: function(el){
+// 			new Vue(Object.assign({
+// 				el: el,
+// 			}, require('./game1.vue')));
+// 		}
+// 	}
+// ];
 
-var vm = new Vue({
-    el: '#body',
-    data: function(){
-    	return {
-			panes: panes
-		}
-    },
-	ready: function(){
-		var body = document.getElementById('body');
-		body.style.height = (document.documentElement.clientHeight) + 'px';
-	},
-    components: {
-        'carousel': require('../../widget/carousel.vue')
-    }
-});
+// var vm = new Vue({
+//     el: '#body',
+//     data: function(){
+//     	return {
+// 			panes: panes
+// 		}
+//     },
+// 	ready: function(){
+// 		var body = document.getElementById('body');
+// 		body.style.height = (document.documentElement.clientHeight) + 'px';
+// 	},
+//     components: {
+//         'carousel': require('../../widget/carousel.vue')
+//     }
+// });
